6e135b7b8de804407cd8cf988b44c2f859fa2d51
Ross Libby <rosslibby92@gmail.com>
2025-12-19 10:57:37 -0500

    built helper function to create an Event payload

--- services/helpers.js
+++ services/helpers.js
@@ -4,4 +4,29 @@ async function getEventById(eventId) {
     .catch((err) => { throw new Error(err) });
 }
 
-module.exports = { getEventById };
+/**
+ * I created this method in case I was NOT supposed to
+ * touch the mock-service.
+ * 
+ * This event-creation method performs validation by
+ * verifying that the passed userId is associated with
+ * an existing user account.
+ */
+async function createEventPayload(userId, name, details) {
+  // ensure the user exists
+  return fetch(`http://event.com/getUserById/${userId}`)
+    .then((res) => {
+      if (res.status === 404) {
+        throw new Error(`Unable to finder user with ID ${userId}`);
+      } else {
+        return res;
+      }
+    }).then(() => {
+      const id = crypto.randomUUID();
+      const event = { id, userId, name, details };
+      return event;
+    })
+}
+
+
+module.exports = { createEventPayload, getEventById };
