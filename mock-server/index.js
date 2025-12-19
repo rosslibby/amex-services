12c569e4a731538c38574661bd473040d20b4b4d
Ross Libby <rosslibby92@gmail.com>
2025-12-19 10:49:27 -0500

    implement refactored stores

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,83 +1,9 @@
 const { http, HttpResponse, delay } = require('msw');
 const { setupServer } = require('msw/node');
-const user = require('./mocks/user.json');
- 
-let requestCount = 0
-const userStore = {
-  users:{
-    "1" : {
-      userName: 'user1',
-      id: 1,
-      email: 'hello@gmail.com',
-      events: ['event-1', 'event-3']
-    },
-    "2" : {
-      userName: 'user2',
-      id: 2,
-      email: 'hello2@gmail.com',
-      events: ['event-2']
-    },
-    "3" : {
-      userName: 'user3',
-      id: 2,
-      email: 'hello3@gmail.com',
-      events: ['event-4']
-    }
-  },
-  addUser(user) {
-    this.users[user.id] = user;
-    return user;
-  },
-  getUser(id) {
-    return this.users[id];
-  },
-  getUsers() {
-    return Object.values(this.users);
-  },
-  addEventTouUser(userId, eventId) {
-    this.users[userId].events.push(eventId);
-  }
-}
+const { userStore, eventStore } = require('./stores');
+const users = require('./mocks/user.json');
 
-const eventStore = {
-  events: {
-    "event-1": {
-      id: 1,
-      name: 'Event 1',
-      userId: 1,
-      details: 'This is the first event'
-    },
-    "event-2": {
-      id: 1,
-      name: 'Event 2',
-      userId: 2,
-      details: 'This is the first event'
-    },
-    "event-3": {
-      id: 1,
-      name: 'Event 1',
-      userId: 1,
-      details: 'This is the first event'
-    },
-    "event-4": {
-      id: 1,
-      name: 'Event 4',
-      userId: 3,
-      details: 'This is the first event'
-    }
-  },
-  addEvent(event) {
-    this.events[event.id] = event;
-    return event;
-  },
-  getEvent(id) {
-    return this.events[id];
-  },
-  getEvents() {
-    return Object.values(this.events);
-  }
-};
-
+let requestCount = 0;
 
 // Provide the server-side API with the request handlers.
 const server = setupServer(
@@ -97,8 +23,8 @@ const server = setupServer(
     // Simulate a successful response for the first 5 requests
     if (requestCount <= 5 || requestCount == 0) {
       const requestBody = await request.json()
-      eventStore.addEvent(requestBody);
-      userStore.addEventTouUser(requestBody.userId, requestBody.id)
+      const { id: eventId } = eventStore.addEvent(requestBody);
+      userStore.addEventToUser(requestBody.userId, eventId);
       return HttpResponse.json({
         success: true
       });
@@ -128,11 +54,11 @@ const server = setupServer(
     await delay(500);
     return HttpResponse.json(eventStore.getEvent(params.id))
   }),
-)
+);
 
 const listenMock = () => {
   // Start the interception.
-  server.listen()
-}
+  server.listen();
+};
 
 module.exports = listenMock;
